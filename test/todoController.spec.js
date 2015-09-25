b00bc61afd87450e46506943c3edb62d9b695ba7
jonathansayer <jon.sayer1@gmail.com>
2015-09-25 10:27:17 +0100

    added adds a chore to a chores array unit test

--- test/todoController.spec.js
+++ test/todoController.spec.js
@@ -12,11 +12,20 @@ describe("todoListController", function(){
     expect(ctrl.chores).toBeUndefined();
   })
 
-  
+  it('adds a chore to the Chores array', function(){
+    ctrl.chore = "Shopping"
+    ctrl.chores = []
+    ctrl.addToList();
+    expect(ctrl.chores).toEqual([{"name" : "Shopping", "isDone" : false}]);
+  })
+
+  it('is able to change a chore from being not Done to Done', function(){
+    ctrl.chore = "Shopping"
+    ctrl.chores = []
+    ctrl.addToList();
+    ctrl.choreComplete(ctrl.chores[0])
+    expect(ctrl.chores[0].isDone).toEqual(true)
+  })
 
 
 })
-
-
-
-
